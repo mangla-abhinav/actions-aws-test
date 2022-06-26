7548403f00675c9240e128745e0e55494efdd174
Abhinav Mangla <abhinavmangla.india@gmail.com>
2022-06-26 22:10:44 +0530

    mvjgh

--- piano-keyboard/app.js
+++ piano-keyboard/app.js
@@ -1,9 +1,9 @@
 $(document).ready(() => {
-  const keys = $('a');
+  var keys = $('a');
   const sounds = [];
   for (let i = 0; i < keys.length; i += 1) {
     sounds[i] = new Howl({
-      src: [`audio/key-${i + 1}.mp3`],
+      src: [`audio/key-${i + 1}.mp3`]
     });
     $(keys[i]).click((e) => {
       e.preventDefault();
